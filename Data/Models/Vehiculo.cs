18eb5af130983b5dcacb575ff1e95a180529e6ed
unknown <2020-0091@uteco.edu.do>
2022-11-02 12:47:59 -0400

    first commit

---
+++ Data/Models/Vehiculo.cs
@@ -0,0 +1,10 @@
+namespace Data.Models;
+
+public class Vehiculo
+{
+    public int vehiculoID { get; set; }
+    public string? Marca { get; set; }
+    public string? Modelo { get; set; }
+    public int Año { get; set; }
+    public string? Color { get; set; }
+}
